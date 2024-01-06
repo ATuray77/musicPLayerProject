c88f3bd2bd4d12790e63f9fa61806e6084cf0a92
Aruna Turay <tura_arun@yahoo.com>
2024-01-06 11:36:36 -0500

    removes notes

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// const audio = document.getElementById('audio');
-// const play = document.getElementById('play');
-// const pause = document.getElementById('pause');
-// const stop = document.getElementById('stop');
-// const currentTime = document.getElementById('current-time');
-// const volume = document.getElementById('volume');
-
-// play.addEventListener('click', () => audio.play());
-// pause.addEventListener('click', () => audio.pause());
-// stop.addEventListener('click', () => {
-//     audio.pause();
-//     audio.currentTime = 0;  //pause then reset the current time prop
-
-// });
-
-// //adds eventlistener to show the current time updates
-// audio.addEventListener('timeupdate', () => {
-//     currentTime.innerHTML = audio.currentTime;
-// });
-
-// //adds eventlistener to show the volume range
-// volume.addEventListener('change', () => (audio.volume = volume.value));
 
 const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
